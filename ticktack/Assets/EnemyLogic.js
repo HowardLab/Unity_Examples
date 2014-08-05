a07b8614b8affa7b4aca56c724fa450cd76da767
CHAZICLE <chazicle@gmail.com>
2014-08-05 12:09:00 +0100

    Added tick tack example

---
+++ ticktack/Assets/EnemyLogic.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var health : int = 100;
+var dieing : boolean = false;
+var isDead : boolean = false;
+
+function doDamage(dmg : int)
+{
+	health -= dmg;
+	if(health<=0 && !isDead)
+	{
+		isDead = true;
+		animation.Play("EnemyDeath");
+	}
+}
---
+++ ticktack/Assets/MeleeSystem.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var dmg : int = 50;
+var distance : float;
+var maxDistance : float = 1.5;
+var mace : Transform;
+
+function Update()
+{
+	if(Input.GetButtonUp("Fire1"))
+	{
+	mace.animation.Play("Attack");
+		var hit : RaycastHit;
+		if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), hit))
+		{
+			hit.transform.collider.rigidbody.AddForce(Vector3.forward);
+			distance = hit.distance;
+			if (distance < maxDistance)
+			{
+				hit.transform.SendMessage("doDamage",dmg,SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
+}
