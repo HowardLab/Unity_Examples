318c474b268872bb18837eaaa9238ade70d90c9d
drianhoward <drianhoward@gmail.com>
2014-10-05 16:49:03 +0100

    someemssage

---
+++ TickTackUnity/Assets/enemyLogic.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+var hit = false;
+
+function Start () {
+
+}
+
+function Update () {
+	
+}
+
+function Punch() {
+	if(!hit)
+	{
+		hit = true;
+		animation.Play("punch");
+		animation["punch"].speed = 1;
+		animation["punch"].time = 0;
+	}
+}
+
+function Reset() {
+	if(hit)
+	{
+		hit = false;
+		animation.Play("punch");
+		animation["punch"].speed = -1;
+		animation["punch"].time = 0;
+	}
+}
